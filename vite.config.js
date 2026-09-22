08266eff0a11cbe414978f2a374fc95ce5e6285d
davidsapp <dakama254@gmail.com>
2026-09-22 21:09:16 +0300

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+base: './',
+
+server: {
+host: '0.0.0.0',
+port: 5173
+},
+
+build: {
+outDir: 'dist',
+assetsDir: 'assets',
+sourcemap: false
+}
+});
